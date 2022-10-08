d5b92f2e2c489b3a75d402e7d99dad7f5c63bbe4
wallace08797 <fenilahir16@gmail.com>
2022-10-09 01:00:51 +0530

    Welcome Screen Up and Running

--- app/src/main/java/com/trackerl/tasktracker/MainActivity.kt
+++ app/src/main/java/com/trackerl/tasktracker/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.trackerl.tasktracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        val go: Button =findViewById(R.id.button)
+
+        go.setOnClickListener {
+            gotohome()
+        }
+
+
+    }
+
+    fun gotohome(){
+        val intent:Intent=Intent(this,Home::class.java)
+        startActivity(intent)
+
+        overridePendingTransition(R.anim.enteractivity,R.anim.enteractivity)
     }
 }
